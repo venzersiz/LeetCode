6abbfbb3f4d3549693072671d9cb6cb2ffba67be
krevis <venzersiz@gmail.com>
2021-09-11 10:38:11 +0900

    Time: 10 ms (10.45%), Space: 38.8 MB (34.79%) - LeetHub

--- longest-common-prefix/longest-common-prefix.java
+++ longest-common-prefix/longest-common-prefix.java
@@ -5,7 +5,7 @@ class Solution {
         Arrays.sort(strs, Comparator.comparingInt(String::length));
 
         List<Character> chars = new ArrayList<>();
-        int commonPrefixIndex = 0;
+        int lastIndexOfCommonPrefix = 0;
 
         for (int i = 0; i < strs.length; i++) {
             String str = strs[i];
@@ -14,20 +14,20 @@ class Solution {
                 for (int j = 0; j < str.length(); j++) {
                     chars.add(str.charAt(j));
                 }
-                commonPrefixIndex = chars.size() - 1;
+                lastIndexOfCommonPrefix = chars.size() - 1;
             } else {
-                for (int j = 0; j <= commonPrefixIndex; j++) {
+                for (int j = 0; j <= lastIndexOfCommonPrefix; j++) {
                     if (str.charAt(j) != chars.get(j)) {
-                        commonPrefixIndex = j - 1;
+                        lastIndexOfCommonPrefix = j - 1;
                         break;
                     }
                 }
-
             }
         }
 
-        List<Character> output = chars.subList(0, commonPrefixIndex + 1);
-
-        return output.stream().map(String::valueOf).collect(Collectors.joining());
+        return chars.subList(0, lastIndexOfCommonPrefix + 1)
+                    .stream()
+                    .map(String::valueOf)
+                    .collect(Collectors.joining());
     }
 }
